dd1ad17147438d2c4b931bb39f86210381b0c27b
Replit user <>
2023-03-03 18:05:09 -0700

    Compose and connect pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,18 @@
 // import logo from './logo.svg';
 import './App.css';
-import Inbox from './components/Inbox';
+import PageDisplay from './pages/PageDisplay';
 import Sidebar from './components/Sidebar';
+import { Route, Routes } from 'react-router-dom';
+import ConnectPage from './pages/ConnectPage';
 function App() {
   return (
     <div className="App">
       <Sidebar />
-      <Inbox />
+      <PageDisplay />
+      {/* <Routes>
+        <Route path='/' element={<PageDisplay />}/>
+        <Route path='/connect' element={<ConnectPage />}/>
+      </Routes> */}
     </div>
   );
 }
---
+++ client/src/pages/ConnectPage.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { useConnect } from 'wagmi'
+
+
+function ConnectPage() {
+
+    const { connect, connectors, error, isLoading, pendingConnector } =
+    useConnect();
+    return (
+        <div>
+            {connectors.map((connector) => (
+                <button
+                    disabled={!connector.ready}
+                    key={connector.id}
+                    onClick={() => connect({ connector })}
+                >
+                    {connector.name}
+                    {!connector.ready && ' (unsupported)'}
+                    {isLoading &&
+                        connector.id === pendingConnector?.id &&
+                        ' (connecting)'}
+                </button>
+            ))}
+
+            {error && <div>{error.message}</div>}
+        </div>
+    );
+}
+
+export default ConnectPage;
