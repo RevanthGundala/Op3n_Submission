ff7d3a261a23546f4cd3ad54d5125bc43884a308
Replit user <>
2023-03-04 14:12:20 -0700

    Got working routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,22 @@
 // import logo from './logo.svg';
 import './App.css';
-import PageDisplay from './pages/PageDisplay';
 import Sidebar from './components/Sidebar';
-import { Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import ConnectPage from './pages/ConnectPage';
+import InboxPage from './pages/InboxPage';
 function App() {
   return (
-    <div className="App">
-      <Sidebar />
-      <PageDisplay />
-      {/* <Routes>
-        <Route path='/' element={<PageDisplay />}/>
-        <Route path='/connect' element={<ConnectPage />}/>
-      </Routes> */}
-    </div>
+    <Router>
+      <div className="App">
+        <Sidebar />
+        <section>
+          <Routes>
+            <Route path='/' element={<InboxPage />} />
+            <Route path='/' element={<ConnectPage />} />
+          </Routes>
+        </section>
+      </div>
+    </Router>
   );
 }
 
