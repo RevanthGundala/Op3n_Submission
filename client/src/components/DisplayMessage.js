ae83e3558f4ad8e605fd74ee78fcb88df11caed7
Replit user <>
2023-03-04 21:28:20 -0700

    Display message styling

--- client/src/components/DisplayMessage.js
+++ client/src/components/DisplayMessage.js
@@ -3,7 +3,7 @@ import React from "react";
 function DisplayMessage(props) {
     const {content, subject, sender, date} = props.messageData
     return (
-        <div className="DisplayMessage-container">
+        <div className="displayMessage-container">
             <h3>{subject}</h3>
             <h4>{date}</h4>
             <div>From: {sender}</div>
