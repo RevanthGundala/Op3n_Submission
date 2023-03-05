0373209be203c75f9f1166e7e383e22816837b90
Replit user <>
2023-03-04 20:12:38 -0700

    Added selection functionality

--- client/src/components/Sidebar.js
+++ client/src/components/Sidebar.js
@@ -1,14 +1,14 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 function Sidebar() {
     return (
         <div className="sidebar">
             <div className="logo">LOGO</div>
             <div className="navlist">
-                <div className="navlink"><Link to='/'><i>[]</i>Messages</Link></div>
-                <div className="navlink"><Link to='/'><i>[]</i>History</Link></div>
-                <div className="navlink"><Link to='/'><i>[]</i>Settings</Link></div>
+                <div className="navlink"><NavLink to='/'><i>[]</i>Messages</NavLink></div>
+                <div className="navlink"><NavLink to='/history'><i>[]</i>History</NavLink></div>
+                <div className="navlink"><NavLink to='/connect'><i>[]</i>Settings</NavLink></div>
             </div>
         </div>
     );
