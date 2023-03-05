107c076f43122cb6b213c25e23dea496631b2726
Replit user <>
2023-03-04 20:04:16 -0700

    Created View Message page

---
+++ client/src/pages/ViewMessagePage.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+import DisplayMessage from "../components/DisplayMessage";
+
+function ViewMessagePage() {
+    const { msgAddress } = useParams();
+    //GET MESSAGE DATA USING ADDRESS
+    msgData = {
+        address: 'ADDRESS',
+        sender: 'SENDER',
+        date: 'DATE',
+        subject: 'SUBJECT',
+        content: 'CONTENT'
+    }
+
+    return (
+        <>
+            <div>
+                <h2>View Message</h2>
+                <button>Reply <i>[]</i></button>
+            </div>
+            <DisplayMessage messageData={msgData}/>
+        </>
+    );
+}
+
+export default ViewMessagePage;
